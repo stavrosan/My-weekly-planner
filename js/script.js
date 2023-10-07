0902eef8caf8f3d5ebd15d520e7cffe76570da8d
Stavros Anagnostakis <stavrosanagno@gmail.com>
2023-10-07 16:48:50 +0200

    Final

--- js/script.js
+++ js/script.js
@@ -10,14 +10,17 @@ taskList.forEach(item => {
     
     document.getElementById("result").innerHTML +=
      `
-    <div class="card text-center p-3" style:"max-width: 100%;">
-    <img src="${item.image}" class="card-img-top img-fluid max-width:100% h-100 ">
+    <div class="card text-center p-3 mx-auto" style:"max-width:100%;">
+    <img src="${item.image}" class="card-img-top img-fluid max-width-100 h-100">
     <div class="card-body">
-      <h3 class="card-title" style ="color :">${item.taskName}</h3>
+      <h3 class="card-title">${item.taskName}</h3>
       <h5 class="card-text">${item.description}</h5>
+      <hr>
       <p>Priority level: 
       <button class="btn btn-success myBtn"><span class="import">${item.importance}</span></button></p>
+      <hr>
       <p>Day and Time: ${item.dayTime} <br> Expected duration: ${item.duration} hour(s).</p>
+      <button class="btn btn-success"> Done </button> <button class="btn btn-danger">Delete</button>
       </div>
       </div>
 `;
@@ -39,8 +42,8 @@ btn.addEventListener("click", function () {
 })
 );
  
-//function that stops counting on 5 and 1 index num 
-
+//function that stops counting on 5 and +1 index num 
+let color = "";
 
 function inImportance(i) {
     if (taskList[i].importance<5){   
@@ -53,15 +56,16 @@ function inImportance(i) {
 //function to change the color of the importance button on different count clicks
 
 function bagColor(importance) {
-if (importance = 2 || 3) {
+if (importance == 2 || 3) {
     return "btn-warning";
-} else if (importance = 4 || 5) {
+} else if (importance == 4 || 5) {
     return "btn-danger";
 }
     else{
         return "btn-success";
 }
 } 
+console.log(bagColor);
 
 //Sorting items button from most clicks to less clicks. 
 //forEach loop for every task(item) and get id(result) from container in HTML
@@ -76,14 +80,17 @@ document.getElementById("btnSort").addEventListener("click", function () {
     
         document.getElementById("result").innerHTML +=
          `
-        <div class="card text-center p-3" style:"max-width: 100%;">
-        <img src="${item.image}" class="card-img-top img-fluid max-width:100% h-100">
+        <div class="card text-center p-3 mx-auto" style:"max-width:100%;">
+        <img src="${item.image}" class="card-img-top img-fluid max-width-100 h-100">
         <div class="card-body">
-          <h3 class="card-title" style ="color :">${item.taskName}</h3>
+          <h3 class="card-title">${item.taskName}</h3>
           <h5 class="card-text">${item.description}</h5>
+          <hr>
           <p>Priority level:
           <button class="btn btn-success myBtn"><span class="import">${item.importance}</span></button></p>
+          <hr>
           <p>Day and Time: ${item.dayTime} <br>Expected duration: ${item.duration} hour(s).</p>
+          <button class="btn btn-success"> Done </button> <button class="btn btn-danger">Delete</button>
           </div>
       </div>
     `;
