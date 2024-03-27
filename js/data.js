cd32cf3c554755aa161510ab4449f8b5ab3b07ba
Stavros Anagnostakis <stavrosanagno@gmail.com>
2024-03-27 21:40:58 +0100

    imgs bug

--- js/data.js
+++ js/data.js
@@ -1,62 +1,62 @@
 let tasks = `[{
     "taskName":"Code Factory course",
-    "image":"../Images/it.jpg",
+    "image":"./Images/it.jpg",
     "description":"Full Stack Developer Course.",
     "dayTime":"Monday-Thursday at 09:00 AM",
     "duration":7,
     "importance":0
 },{
     "taskName":"Dentist",
-    "image":"../Images/dentist.jpg",
+    "image":"./Images/dentist.jpg",
     "description":"Regular monthly appointment.",
     "dayTime":"Tuesday at 05:00 PM",
     "duration": 1,
     "importance":0
 },{
     "taskName":"Shopping",
-    "image":"../Images/groceries.jpg",
+    "image":"./Images/groceries.jpg",
     "description":"Groceries weekly shopping at Spar.",
     "dayTime":"Friday at 05:00 PM",
     "duration": 2,
     "importance":0
 },{
     "taskName":"Vet",
-    "image":"../Images/dogvet.jpg",
+    "image":"./Images/dogvet.jpg",
     "description":"Take dog to the Vet.",
     "dayTime":"Monday at 06:00 PM",
     "duration": 2,
     "importance":0
 },{
     "taskName":"German",
-    "image":"../Images/german.jpg",
+    "image":"./Images/german.jpg",
     "description":"German language lessons.",
     "dayTime":"Thursday-Saturday at 07:00 PM",
     "duration": 3,
     "importance":0
 },{
     "taskName":"Interview",
-    "image":"../Images/jobint.jpg",
+    "image":"./Images/jobint.jpg",
     "description":"Job interview at IT_solutions.GMBH.",
     "dayTime":"Friday at 09:00 AM",
     "duration": 1,
     "importance":0
 },{
     "taskName":"Studio",
-    "image":"../Images/studio.jpg",
+    "image":"./Images/studio.jpg",
     "description":"Music rehearsal.",
     "dayTime":"Saturday at 02:00 PM",
     "duration": 3,
     "importance":0
 },{
     "taskName":"Cleaning",
-    "image":"../Images/houseclean.jpg",
+    "image":"./Images/houseclean.jpg",
     "description":"Weekly cleaning of the apartment.",
     "dayTime":"Saturday at 09:00 AM",
     "duration": 4,
     "importance":0
 },{
     "taskName":"Car",
-    "image":"../Images/carservice.jpg",
+    "image":"./Images/carservice.jpg",
     "description":"Car 6-month service.",
     "dayTime":"Wednesday at 06:00 PM",
     "duration": 2,
