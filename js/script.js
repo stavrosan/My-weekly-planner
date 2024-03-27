fde1f3222da6028ab5c295dbed29cef8676eaf0e
Stavros Anagnostakis <stavrosanagno@gmail.com>
2024-03-27 21:05:24 +0100

    fixed js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,9 @@ let taskList = JSON.parse(tasks);
 console.log(taskList);
 //Created objects of a JSON array
 
-
-
 //ForEach loop+=to get all the objects values of the container in HTML file
-taskList.forEach(item => {
-    
-    document.getElementById("result").innerHTML +=
-     `
+taskList.forEach((item) => {
+  document.getElementById("result").innerHTML += `
     <div class="col mb-4">
       <div class="card text-center p-3">
        <div class="d-flex justify-content-between align-items-center">
@@ -31,102 +27,77 @@ taskList.forEach(item => {
       `;
 });
 
-
-
-
-
 //Button to add eventlistener onclick
 
 let btns = document.querySelectorAll(".myBtn");
 
-
-btns.forEach(( btn , i) => 
-
-
-btn.addEventListener("click", function () {
-  
-    inImportance(i)
-})
+btns.forEach((btn, i) =>
+  btn.addEventListener("click", function () {
+    inImportance(i);
+  }),
 );
 
-
- 
-//function that stops counting on 5 and +1 index num 
+//function that stops counting on 5 and +1 index num
 
 function inImportance(i) {
-    if (taskList[i].importance<5){   
+  if (taskList[i].importance < 5) {
     taskList[i].importance++;
     console.log(taskList[i]);
     document.querySelectorAll(".import")[i].innerText = taskList[i].importance;
     btns[i].classList.add(bagColor(taskList[i].importance));
-
+  }
 }
-}
-
-
 
 function doneColor(i) {
-    const cards = document.querySelectorAll(".card");
-    cards[i].style.opacity="0.3";
+  const cards = document.querySelectorAll(".card");
+  cards[i].style.opacity = "0.3";
 }
-
-
 
 let doneBtns = document.querySelectorAll(".done");
 
-doneBtns.forEach((donebtn, i)=>{
-    donebtn.addEventListener('click', ()=>{
-        doneColor(i);
-    })
-})
-
-
+doneBtns.forEach((donebtn, i) => {
+  donebtn.addEventListener("click", function () {
+    doneColor(i);
+  });
+});
 
 //Function that deletes card with the same delete button
 function removeTask(i) {
-   taskList.splice(i,1);
+  taskList.splice(i, 1);
 }
 
 let removeBtns = document.querySelectorAll(".remove");
 
-   removeBtns.forEach((removeBtn,i) => {
-   removeBtn.addEventListener("click",function(){
-     
+removeBtns.forEach((removeBtn, i) => {
+  removeBtn.addEventListener("click", function () {
     this.closest("div.col").remove();
-     removeTask(i);
-   
-      })
-})
-
-
+    removeTask(i);
+  });
+});
 
 //function to change the color of the importance button on different count clicks
 function bagColor(importance) {
-    if (importance == 2 && 3) {
-        return "btn-warning";
-    } else if (importance == 4 && 5) {
-        return "btn-danger";
-    }
-        else{
-            return "btn-success";
-    }
-    } 
-
+  if (importance == 2 && 3) {
+    return "btn-warning";
+  } else if (importance == 4 && 5) {
+    return "btn-danger";
+  } else {
+    return "btn-success";
+  }
+}
 
 //console.log(bagColor);
 
-//Sorting items button from most clicks to less clicks. 
+//Sorting items button from most clicks to less clicks.
 //forEach loop for every task(item) and get id(result) from container in HTML
 
 document.getElementById("btnSort").addEventListener("click", function () {
-    let newTask = taskList.sort((a, b) => b.importance - a.importance);
-    console.log(newTask);
+  let newTask = taskList.sort((a, b) => b.importance - a.importance);
+  console.log(newTask);
 
-    document.getElementById("result").innerHTML ="";
-    newTask.forEach(item => {
-       
-     document.getElementById("result").innerHTML +=
-      `
+  document.getElementById("result").innerHTML = "";
+  newTask.forEach((item) => {
+    document.getElementById("result").innerHTML += `
     <div class="col mb-4">
      <div class="card text-center p-3">
       <div class="d-flex justify-content-between align-items-center">
@@ -148,6 +119,5 @@ document.getElementById("btnSort").addEventListener("click", function () {
     </div>
       
     `;
-    });
-
-})
+  });
+});
